ff21b94ed0f87024f66396399faa9b1be216a645
Gert Cuykens <gert.cuykens@gmail.com>
2013-07-25 03:24:39 +0200

    token

--- src/Client.hs
+++ src/Client.hs
@@ -5,7 +5,7 @@ import Data.Acid.Advanced    ( query', update' )
 import Data.Acid.Remote      ( openRemoteState, sharedSecretPerform )
 import Data.ByteString.Char8 ( pack )
 import Network               ( PortID(PortNumber) )
-import Token                 ( create, verify)
+import Token                 ( create, verify )
 import Table                 ( GroupMap(..), Group(..), InsertKey(..), LookupKey(..), group)
 
 openAcidState :: IO (AcidState GroupMap)
@@ -17,14 +17,17 @@ runAcidState acid = do
     _ <- update' acid (InsertKey 1 (Group [116469479527388802962]))
     _ <- update' acid (InsertKey 2 (Group [116469479527388802962]))
     _ <- update' acid (InsertKey 3 (Group [116469479527388802962]))
+
     Just p <- query' acid (LookupKey 0)
     print (116469479527388802962 `elem` group p)
     print (555 `elem` group p)
     print (666 `elem` group p)
-    t <- create 116469479527388802962 555
+
+    t <- create 116469479527388802962 [0,1,2,3]
     print t
     b <- verify t
     print b
+
     createCheckpoint acid
 
 main :: IO ()
--- src/Token.hs
+++ src/Token.hs
@@ -3,22 +3,27 @@ import Codec.Utils           ( Octet, toTwosComp )
 import Data.HMAC             ( hmac_sha1 )
 import Data.UnixTime         ( getUnixTime, utSeconds )
 
-data Token = Token {seconds::Int, userId::Int, groupId::Int, hash::[Octet]} deriving (Show)
+data Token = Token {seconds::Int, userId::Int, groupId::[Int], hash::[Octet]} deriving (Show)
 
 secret :: [Octet]
 secret = [48,57]
+
+groupOctet :: [Int] -> [Octet]
+groupOctet [] = []
+groupOctet [x] = toTwosComp x
+groupOctet (x:xs) = groupOctet xs ++ toTwosComp x
 
 verify :: Token -> IO Bool
 verify t = do
     u <- getUnixTime
     let s = fromEnum (utSeconds u) - seconds t
-    let h = toTwosComp (seconds t) ++ toTwosComp (userId t) ++ toTwosComp (groupId t)
+    let h = toTwosComp (seconds t) ++ toTwosComp (userId t) ++ groupOctet (groupId t)
     return $ (s <= 3600) && (hmac_sha1 secret h == hash t)
 
-create :: Int -> Int -> IO Token
+create :: Int -> [Int] -> IO Token
 create i1 i2 = do
     u <- getUnixTime
     let s = fromEnum (utSeconds u)
-    let h = toTwosComp s ++ toTwosComp i1 ++ toTwosComp i2
+    let h = toTwosComp s ++ toTwosComp i1 ++ groupOctet i2
     return $ Token s i1 i2 (hmac_sha1 secret h)
 
