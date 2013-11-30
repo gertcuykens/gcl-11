e5e392bdde50d89ae09ed77752d6bf2f76c64ddc
Gert Cuykens <gert.cuykens@gmail.com>
2013-11-30 05:55:25 +0100

    android

--- android/src/my/endpoints/EndpointsActivity.java
+++ android/src/my/endpoints/EndpointsActivity.java
@@ -6,38 +6,75 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
-import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.json.gson.GsonFactory;
 
 public class EndpointsActivity extends Activity implements View.OnClickListener {
-    private GoogleAccountCredential credential;
+    private String AUDIENCE = "server:client_id:522156758812-09f5qv0e4gqjdjqfocerqcud5m5jutau.apps.googleusercontent.com";
+    private GoogleAccountCredential user;
+    private EndpointsClient service;
     private TextView userStatus;
+    private EditText getGreetingValue;
+    private EditText greetingValue;
+    private EditText multiplyValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.endpoints);
 
-        String AUDIENCE = "server:client_id:522156758812-09f5qv0e4gqjdjqfocerqcud5m5jutau.apps.googleusercontent.com";
-        credential = GoogleAccountCredential.usingAudience(this, AUDIENCE);
-        credential.setSelectedAccountName("gert.cuykens@gmail.com");
-        //credential = null;
+        user = GoogleAccountCredential.usingAudience(this, AUDIENCE);
+
+        EndpointsClient.Builder endpoints = new EndpointsClient.Builder(AndroidHttp.newCompatibleTransport(), new GsonFactory(), null);
+        service = endpoints.build();
 
         userStatus = (TextView) findViewById(R.id.userStatus);
-        userStatus.setText("gert.cuykens@gmail.com");
+        getGreetingValue = (EditText) findViewById(R.id.getGreetingValue);
+        greetingValue = (EditText) findViewById(R.id.greetingValue);
+        multiplyValue = (EditText) findViewById(R.id.multiplyValue);
 
         Button userButton = (Button) findViewById(R.id.userButton);
         userButton.setOnClickListener(this);
 
         Button getGreetingButton = (Button) findViewById(R.id.getGreetingButton);
         getGreetingButton.setOnClickListener(this);
+
+        Button getListButton = (Button) findViewById(R.id.getListButton);
+        getListButton.setOnClickListener(this);
+
+        Button multiplyButton = (Button) findViewById(R.id.multiplyButton);
+        multiplyButton.setOnClickListener(this);
+
+        Button authenticatedButton = (Button) findViewById(R.id.authenticatedButton);
+        authenticatedButton.setOnClickListener(this);
+
+        Button soapButton = (Button) findViewById(R.id.soapButton);
+        soapButton.setOnClickListener(this);
+
+        Button datastoreButton = (Button) findViewById(R.id.datastoreButton);
+        datastoreButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Context context = view.getContext();
+        switch(view.getId()) {
+            case R.id.userButton: startActivityForResult(user.newChooseAccountIntent(),1); break;
+            case R.id.getGreetingButton: new RestTask().execute(Pair.create(context, 1)); break;
+            case R.id.getListButton: new RestTask().execute(Pair.create(context, 2)); break;
+            case R.id.multiplyButton: new RestTask().execute(Pair.create(context, 3)); break;
+            case R.id.authenticatedButton: new RestTask().execute(Pair.create(context, 4)); break;
+            case R.id.soapButton: new RestTask().execute(Pair.create(context, 5)); break;
+            case R.id.datastoreButton: new RestTask().execute(Pair.create(context, 6)); break;
+        }
     }
 
     @Override
@@ -48,56 +85,36 @@ public class EndpointsActivity extends Activity implements View.OnClickListener 
                 if (data != null && data.getExtras() != null) {
                     String accountName = data.getExtras().getString(AccountManager.KEY_ACCOUNT_NAME);
                     if (accountName != null) {
-                        credential.setSelectedAccountName(accountName);
-                        userStatus.setText(credential.getSelectedAccountName());
+                        user.setSelectedAccountName(accountName);
+                        userStatus.setText(user.getSelectedAccountName());
+                        EndpointsClient.Builder endpoints = new EndpointsClient.Builder(AndroidHttp.newCompatibleTransport(), new GsonFactory(), user);
+                        service = endpoints.build();
                     }
                 }
                 break;
         }
     }
 
-    @Override
-    public void onClick(View view) {
-        Context context = view.getContext();
-        switch(view.getId()) {
-            case R.id.userButton:
-                startActivityForResult(credential.newChooseAccountIntent(),1);
-                break;
-            case R.id.getGreetingButton:
-                new RestTask(context).execute();
-                break;
-        }
-    }
-
-    private class RestTask extends AsyncTask<Void, Void, String> {
-        private Context mContext;
-        public RestTask(Context context) {mContext = context;}
+    private class RestTask extends AsyncTask<Pair<Context,Integer>, Void, Pair<Context,String>> {
         @Override
-        protected String doInBackground(Void... unused) {
+        protected Pair doInBackground(Pair<Context,Integer>... p) {
             String text = null;
             try {
-                EndpointsClient.Builder endpoints = new EndpointsClient.Builder(AndroidHttp.newCompatibleTransport(), new GsonFactory(), credential);
-                EndpointsClient service = endpoints.build();
-
-                //Message message = new Message();
-                //message.setMessage("hello ");
-                //Message response = service.get("response/0").execute();
-                //Message response = service.get("response").execute();
-                //Message response = service.post("response/2",message).execute();
-                Message response = service.post("greetings/authed", null).execute();
-                //Message response = service.get("greetings/soap").execute();
-                //Message response = service.get("greetings/datastore").execute();
-
-                text=response.getMessage();
-                //text=response.getItems().toString();
-
+                switch(p[0].second) {
+                    case 1: text = service.get("response/"+getGreetingValue.getText()).execute().getMessage(); break;
+                    case 2: text = service.get("response").execute().getItems().toString(); break;
+                    case 3: text = service.post("response/"+multiplyValue.getText(), new Message().setMessage(""+greetingValue.getText())).execute().getMessage(); break;
+                    case 4: text = service.post("greetings/authed", null).execute().getMessage(); break;
+                    case 5: text = service.get("greetings/soap").execute().getMessage(); break;
+                    case 6: text = service.get("greetings/datastore").execute().getMessage(); break;
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            return text;
+            return Pair.create(p[0].first, text);
         }
         @Override
-        protected void onPostExecute(String text) {toaster(mContext, text);}
+        protected void onPostExecute(Pair<Context,String> p) {toaster(p.first, p.second);}
     }
 
     private void toaster(Context c, String s) {
