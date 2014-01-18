dfb88c412897db7ff2ce083aa4b4946254d389cc
Gert Cuykens <gert.cuykens@gmail.com>
2014-01-18 21:24:37 +0100

    bash

--- iab.sh
+++ iab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-client_id="1093123836049-ilqfjb2s2tqal2fobuduj8b790hnnfju.apps.googleusercontent.com"
+client_id=$GCL_13_ID
 client_secret=$GCL_13_SECRET
 iab_token=$GCL_13_IAB
 
@@ -7,9 +7,7 @@ curl -X GET "https://www.googleapis.com/androidpublisher/v1.1/applications/com.a
     -H "Authorization:  Bearer $1"
 
 #curl -s -X GET "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=$1"
-
 #curl -s -X GET "https://www.googleapis.com/oauth2/v2/userinfo?access_token=$1"
-
 #curl -s -v -X POST "https://gcl-11.appspot.com/_ah/api/rest1/0/greetings/authed" \
 #        -H "Authorization:  Bearer $1" \
 #        -H "Content-Type: application/json; charset=UTF-8" \
--- token.sh
+++ token.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-client_id="1093123836049-ilqfjb2s2tqal2fobuduj8b790hnnfju.apps.googleusercontent.com"
+client_id=$GCL_13_ID
 client_secret=$GCL_13_SECRET
-#redirect_uri="http://localhost:8080/oauth2callback"
-redirect_uri="http%3A%2F%2Flocalhost%3A8080%2Foauth2callback"
-#force="&approval_prompt=force"
-force=""
 
-echo "https://accounts.google.com/o/oauth2/auth?scope=https://www.googleapis.com/auth/androidpublisher&response_type=code&access_type=offline&redirect_uri=$redirect_uri&client_id=$client_id$force"
+echo "https://accounts.google.com/o/oauth2/auth?scope=https://www.googleapis.com/auth/androidpublisher&response_type=code&access_type=offline&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&client_id=$client_id&approval_prompt=force"
 
 read code
 
 curl -X POST "https://accounts.google.com/o/oauth2/token"\
     -H "Content-type: application/x-www-form-urlencoded"\
-    -d "grant_type=authorization_code&code=$code&client_id=$client_id&client_secret=$client_secret&redirect_uri=$redirect_uri"
+    -d "grant_type=authorization_code&code=$code&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&client_id=$client_id&client_secret=$client_secret"
+
