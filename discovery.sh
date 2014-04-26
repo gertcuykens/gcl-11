9edbb5d6c3fbeccf47871bf3f9c105833b634239
Gert Cuykens <gert.cuykens@gmail.com>
2014-04-26 20:14:42 +0200

    clean

--- discovery.sh
+++ discovery.sh
@@ -2,7 +2,3 @@
 URL='https://gcl-11.appspot.com/_ah/api/discovery/v1/apis/rest1/0/rest'
 curl -s $URL > discovery.zip
 endpointscfg.py gen_client_lib java discovery.zip
-
-#URL='https://gcl-13.appspot.com/_ah/api/discovery/v1/apis/rest/v0/rest'
-#curl -s $URL > discovery.zip
-#endpointscfg.py gen_client_lib java discovery.zip
--- token.sh
+++ token.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-client_id="...apps.googleusercontent.com"
-client_secret="..."
+client_id=""
+client_secret=""
+redirect="urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob"
 
 scope="https://www.googleapis.com/auth/androidpublisher+https://www.googleapis.com/auth/devstorage.full_control+https://www.googleapis.com/auth/userinfo.email"
-redirect="urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob"
 
 echo "https://accounts.google.com/o/oauth2/auth?scope=$scope&response_type=code&access_type=offline&redirect_uri=$redirect&client_id=$client_id&approval_prompt=force"
 
